e6ed94eb4a838bc40bd93205ea685f0aef671308
Ayar Maw <ayarlwinmaw@gmail.com>
2025-10-16 17:23:39 -0700

    improve the app by adding html email type instead of plain text

--- main.py
+++ main.py
@@ -26,18 +26,34 @@ content = request.json()
 # print(content["articles"]) # Breakpoint to inspect the json data
 
 # Access the article titles and description
-body = f"Subject:Today News about {topic}!\n"
+body = f""
+
+
 for article in content["articles"][:20]:
     title = article.get("title")
     description = article.get("description")
     article_url = article.get("url")
 
     if title and description:
-        body += (f"{title}\n"
-                 f"{description}\n"
-                 f"{article_url}\n"
-                 f"\n")
+        body += (f"<p><strong>{title}</p></strong>"
+                 f"<p>{description}</p>"
+                 f"<p>{article_url}</p>"
+                 f"<p></p>")
 
-body = body.encode("utf8")
-send_email(message=body)
+# body = body.encode("utf8")
+# send_email(message=body)
 # print(body)
+
+html_body = f"""
+<html>
+  <body>
+    <h2><strong>Here are update news about {topic}!</strong></h2>
+    {body}
+  </body>
+</html>
+"""
+
+message = (f"Subject:{topic} News Digest\n"
+           f"Content-Type: text/html\n\n"
+           f"{html_body}")
+send_email(message=message.encode("utf8"))
